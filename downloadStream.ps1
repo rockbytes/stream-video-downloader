6001a76efe7e0f13372fab7ee99a352e8de7f7ef
Xu Xie <Xu.Xie@serko.com>
2020-03-29 22:06:06 +1300

    Script to download .m3u8 stream video

---
+++ downloadStream.ps1
@@ -0,0 +1,26 @@
+﻿$inputFileName = "input.txt"
+$inputFilePath = [System.IO.Path]::Combine($PSScriptRoot, $inputFileName)
+
+$outputDirName = "output"
+$outputDirPath = [System.IO.Path]::Combine($PSScriptRoot, $outputDirName)
+If(!(test-path $outputDirPath))
+{
+    New-Item -ItemType Directory -Force -Path $outputDirPath
+}
+
+$ffmpegExe = [System.IO.Path]::Combine($PSScriptRoot, "bin", "ffmpeg.exe")
+
+Get-Content $inputFilePath -Encoding UTF8 | foreach {
+    $fields = $_ -split '\s+'
+
+    $url = ($fields[0] -split '\?')[0]
+
+    $outputFileName = $fields[1] + ".mp4"
+    $outputFilePath = [System.IO.Path]::Combine($outputDirPath, $outputFileName)
+    If(test-path $outputFilePath)
+    {
+        Remove-Item $outputFilePath -Force
+    }
+
+    & $ffmpegExe -i $url -c copy -bsf:a aac_adtstoasc $outputFilePath
+}
